5b0a9d899508b1ba0abcfb2991ee5ceae10bd43a
mattgellert <mattgellert@gmail.com>
2017-10-15 10:35:46 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function() {
+    $(this).addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(ev) {
+    if (ev.which === 71) {
+      alert('You pressed G fool!')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+  getIt()
 
+  frameIt()
+
+  pressIt()
+
+  submitIt()
 });
